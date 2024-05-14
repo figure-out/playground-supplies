8e1fa1deff255d8b43e372d5ea919a9806c0bf0c
Pete <1419746+peteredhead@users.noreply.github.com>
2024-05-14 21:14:12 +1200

    Add Bark Surround product

--- src/views/products/BarkChipsView.vue
+++ src/views/products/BarkChipsView.vue
@@ -6,6 +6,8 @@
     easy to install, making it the ideal choice for schools, parks and residential playgrounds.
   </p>
 
+  <p>We also supply wooden surrounds for your bark chip areas made from sustainably sourced Radiata wood.</p>
+
   <p>Keep your little ones safe and happy with our top-of-the-line bark chip today!</p>
   <product-component v-for="product in products" :key="product.sku" :product="product" />
 </template>
@@ -31,6 +33,22 @@ const products: Array<Product> = [
     saleUnit: 'Scoop 1/3M<sup>3</sup>',
     unitPrice: 5000,
     image: '/images/image2.jpg',
+  },
+  {
+    sku: 'BARK_SURROUND_150_4.8',
+    title: 'Radiata wood surround',
+    description: 'Width: 150mm, Thickness 50mm, Length: 4.8m',
+    saleUnit: 'Each 4.8 metre length',
+    unitPrice: 3500,
+    image: '/images/image6.jpg',
+  },
+  {
+    sku: 'BARK_SURROUND_200_6.0',
+    title: 'Radiata wood surround XL',
+    description: 'Width: 200mm, Thickness 50mm, Length: 6.0m',
+    saleUnit: 'Each 6.0 metre length',
+    unitPrice: 5000,
+    image: '/images/image7.jpg',
   },
 ];
 </script>
